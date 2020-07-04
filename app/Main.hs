7db31096dd405a10e1368fdd092bde7562efb96f
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-07-04 20:38:50 +0900

    HTTPアクセスでHello Worldと表示

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai as Wai
+import qualified Network.HTTP.Types as HTypes
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = Warp.run 8000 helloApp
+
+helloApp :: Wai.Application
+helloApp req res = res $ Wai.responseBuilder HTypes.status200 [] "<h1>Hello World!</h1>"
